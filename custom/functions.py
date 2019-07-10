b9f22756ccf80eb5dd0798f20d06a90f566fd3d8
rweidinger <r.weidinger11@gmail.com>
2019-07-10 13:35:36 -0500

    Add files via upload

--- custom/functions.py
+++ custom/functions.py
@@ -57,3 +57,45 @@ class PrevDayHourlyAvgDiffTest1(BaseTransformer):
            description='PrevDayHourlyAvgDiff'
        ))
        return inputs, outputs
+
+class PrevDayHourlyAvgPercentDiffTest1(BaseTransformer):
+   '''
+   Calculate the mean of a data item from yesterday during the same hour as current hour
+   '''
+
+   execute_by = ['deviceid']
+
+   def __init__(self, input_item, output_item='output_item'):
+       self.input_item = input_item
+       self.output_item = output_item
+       super().__init__()
+
+   def _calc(self, df):
+       df = df.copy()
+       today = dt.datetime.today()
+       yesterday = today - dt.timedelta(days=1)
+       yesterday_date = yesterday.date()
+       yesterday_hour = yesterday.hour
+       yesterday_values_hour = df.loc[(df['_timestamp'].dt.date == yesterday_date) &
+                                      (df['_timestamp'].dt.hour == yesterday_hour)][self.input_item]
+       yesterday_values_hour_mean = yesterday_values_hour.mean()
+       df[self.output_item] = ((df[self.input_item] - yesterday_values_hour_mean) / abs(yesterday_values_hour_mean)) * 100
+       return df
+
+   @classmethod
+   def build_ui(cls):
+       # define arguments that behave as function inputs
+       inputs = []
+       inputs.append(ui.UISingleItem(
+           name='input_item',
+           datatype=float,
+           description='Data item to calculate average of same hour of previous day'
+       ))
+       # define arguments that behave as function outputs
+       outputs = []
+       outputs.append(ui.UIFunctionOutSingle(
+           name='PrevDayHourlyAvgDiff',
+           datatype=float,
+           description='PrevDayHourlyAvgDiff'
+       ))
+       return inputs, outputs
